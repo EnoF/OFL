a3ce4bfa33e1e08ae2a252403626c905d3e74a46
Stefan <stefan@jumio.com>
2014-08-12 16:14:14 +0200

    Final stop of code cleanup (Implicit if for won check, separate function for createGame)

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var config = require('./config.js'),
     mysql = require('mysql'),
     connectionpool = mysql.createPool(config.mysql);
 
+function hasWon(goalsTeam1, goalsTeam2) {
+    return ((goalsTeam1 > goalsTeam2) ? true : false);
+};
+
 function sendError(res, err, statCode) {
     console.error('CONNECTION error: ', err);
     res.statusCode = statCode;
@@ -23,7 +27,41 @@ function sendCustomError(res, errMessage, errCode, statCode) {
     });
 };
 
-function createQueryPlayerUpdate (post, points, teams, update) {
+function createGame (connection, req, res, result) {
+    if(result[0] !== undefined && result[0].count === 0) {
+        var post = {
+            team1_player1: req.body.team1[0].id,
+            team1_player2: req.body.team1[1].id,
+            team2_player1: req.body.team2[0].id,
+            team2_player2: req.body.team2[1].id
+        };
+        console.log(post);
+        connection.query('INSERT INTO game SET ?;', post, function postGamesResult(err, result) {
+            if (err) {
+                connection.rollback(function(){
+                   sendError(res, err, 500); 
+                });
+            } else {
+                connection.commit(function(err){
+                    if(err){
+                        connection.rollback(function (){
+                            sendError(res, err, 500);
+                        });
+                    }
+                });
+                res.send({
+                    id: result.insertId
+                });
+            }
+            
+        });
+    } else {
+        console.log(result[0].count);
+        sendCustomError(res, 'Last game is not finished yet!', 'LAST_GAME_UNFINISHED_ERROR', 403);
+    }
+};
+
+function createQueryPlayerUpdate (points, teams, update) {
     var pointsTeam1 = points[0]+points[1];
     var pointsTeam2 = points[2]+points[3];
     var eloMax = 400;
@@ -38,17 +76,13 @@ function createQueryPlayerUpdate (post, points, teams, update) {
 
     console.log(points, difference, gain);
 
-    if(post.team1_goals > post.team2_goals) {
-        teams.team1.won = true;
+    if(teams.team1.won) {
         teams.team1.points = gain;
-        teams.team2.won = false;
         teams.team2.points = -gain;
         update.push('UPDATE player SET points=points+'+teams.team1.points+', victories=victories+1 WHERE id='+teams.team1.players[0]+' OR id='+teams.team1.players[1]+';');
         update.push('UPDATE player SET points=points+'+teams.team2.points+', defeats=defeats+1 WHERE id='+teams.team2.players[0]+' OR id='+teams.team2.players[1]+';');
     } else {
-        teams.team1.won = false;
         teams.team1.points = -gain;
-        teams.team2.won = true;
         teams.team2.points = gain;
         update.push('UPDATE player SET points=points+'+teams.team1.points+', defeats=defeats+1 WHERE id='+teams.team1.players[0]+' OR id='+teams.team1.players[1]+';');
         update.push('UPDATE player SET points=points+'+teams.team2.points+', victories=victories+1 WHERE id='+teams.team2.players[0]+' OR id='+teams.team2.players[1]+';');
@@ -56,7 +90,12 @@ function createQueryPlayerUpdate (post, points, teams, update) {
     console.log(update.join());
 };
 
-function updateGame (connection, req, res, post, teams) {
+function updateGame (connection, req, res, teams) {
+    var post = {
+        team1_goals: teams.team1.goals,
+        team2_goals: teams.team2.goals,
+        finished: 1
+    };
     connection.query('UPDATE game SET ? WHERE id = ?;', [post, req.params.id], function putGamesUpdateResult(err) {
         if (err) {
             connection.rollback(function(){
@@ -78,7 +117,7 @@ function updateGame (connection, req, res, post, teams) {
                     teams.team1.players = [result[0].team1_player1, result[0].team1_player2];
                     teams.team2.players = [result[0].team2_player1, result[0].team2_player2];
 
-                    updatePlayersGetPoints(connection, res, post, playerId, teams);
+                    updatePlayersGetPoints(connection, res, playerId, teams);
                 }
             });
         }
@@ -86,7 +125,7 @@ function updateGame (connection, req, res, post, teams) {
     });
 };
 
-function updatePlayersGetPoints (connection, res, post, playerId, teams) {
+function updatePlayersGetPoints (connection, res, playerId, teams) {
     var points = [];
     var update = [];
     for (var index = 0; index < playerId.length; ++index) {
@@ -97,15 +136,15 @@ function updatePlayersGetPoints (connection, res, post, playerId, teams) {
                 });
             } else {
                 points.push(result[0].points);
-                updatePlayersQuery(connection, res, post, teams, points, update);
+                updatePlayersQuery(connection, res, teams, points, update);
             }
         });
     }
 };
 
-function updatePlayersQuery(connection, res, post, teams, points, update) {
+function updatePlayersQuery(connection, res, teams, points, update) {
     if (points.length===4) {
-        createQueryPlayerUpdate(post, points, teams, update);
+        createQueryPlayerUpdate(points, teams, update);
         console.log(update.join(''));
 
         if(update.length===2) {
@@ -230,70 +269,40 @@ app.post('/games', function postGames(req, res) {
             connection.beginTransaction(function (err) {
                 if (err) {
                     sendError(res, err, 500);
-                }
-                connection.query('SELECT count(finished) as count FROM game WHERE finished = 0;', req.params.id, function postGamesSelectUnfinished(err, result) {
-                    if (err) {
-                        connection.rollback(function (){
-                            sendError(res, err, 500); 
-                        });
-                    } else {
-                        if(result[0] !== undefined && result[0].count === 0) {
-                            var post = {
-                                team1_player1: req.body.team1[0].id,
-                                team1_player2: req.body.team1[1].id,
-                                team2_player1: req.body.team2[0].id,
-                                team2_player2: req.body.team2[1].id
-                            };
-                            console.log(post);
-                            connection.query('INSERT INTO game SET ?;', post, function postGamesResult(err, result) {
-                                if (err) {
-                                    connection.rollback(function(){
-                                       sendError(res, err, 500); 
-                                    });
-                                } else {
-                                    connection.commit(function(err){
-                                        if(err){
-                                            connection.rollback(function (){
-                                                sendError(res, err, 500);
-                                            });
-                                        }
-                                    });
-                                    res.send({
-                                        id: result.insertId
-                                    });
-                                }
-                                connection.release();
+                } else {
+                    connection.query('SELECT count(finished) as count FROM game WHERE finished = 0;', req.params.id, function postGamesSelectUnfinished(err, result) {
+                        if (err) {
+                            connection.rollback(function (){
+                                sendError(res, err, 500); 
                             });
                         } else {
-                            console.log(result[0].count);
-                            sendCustomError(res, 'Last game is not finished yet!', 'LAST_GAME_UNFINISHED_ERROR', 403);
+                            createGame (connection, req, res, result);
                         }
-                    }
-                });
+                    });
+                    connection.release();
+                }
             });
         }
     });
 });
 
 app.put('/games/:id', function putGames(req, res) {
-    var post = {
-        team1_goals: req.body.team1.goals,
-        team2_goals: req.body.team2.goals,
-        finished: 1
-    };
-    var update = [];
-    var teams = {
+        var teams = {
         team1: {
-            won: null,
+            goals: req.body.team1.goals,
+            won: hasWon(req.body.team1.goals, req.body.team2.goals),
             players: null,
             points: null
         },
         team2: {
-            won: null,
+            goals: req.body.team2.goals,
+            won: hasWon(req.body.team2.goals, req.body.team1.goals),
             players: null,
             points: null
         }
-    };   
+    }; 
+    console.log(teams);
+    var update = [];
     connectionpool.getConnection(function(err, connection) {
 
         if (err) {
@@ -311,13 +320,13 @@ app.put('/games/:id', function putGames(req, res) {
                         } else {
                             console.log(result[0]);
                             if (result[0] !== undefined && result[0].finished === 0) {
-                                updateGame (connection, req, res, post, teams);
+                                updateGame (connection, req, res, teams);
                             } else {
                                 sendCustomError(res, 'Game is already in finished state!', 'FINISHED_GAME_ERROR', 403);
                             }
-                            connection.release();
                         }
                     });
+                    connection.release();
                 }
             });
         }
