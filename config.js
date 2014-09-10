75d3a9212ffd5d8421a4094bc630753480c6127f
Stefan <stefan@jumio.com>
2014-09-10 16:16:03 +0200

    Introduced env variables for DB connection

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
 
     mysql: {
-        host: '127.0.0.1',
-        user: 'foosball',
-        password: 'jumio100!',
-        database: 'foosball'
+        host: process.env.LOCAL_HOST,
+        user: process.env.DB_FOOSBALL_USER,
+        password: process.env.DB_FOOSBALL_USER_PW,
+        database: process.env.DB_SCHEMA
     }
 
 }
